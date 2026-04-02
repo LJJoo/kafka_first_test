02a7ceb86c3ec019b9385e332a0db886d5b05361
LJJoo <tlzhsh2@naver.com>
2026-04-02 16:15:16 +0900

    init: kafka-test1, kafka-test2 초기 프로젝트 등록

---
+++ kafka-test1/kafka-test1/src/main/java/com/example/kafka_test1/KafkaTest1Application.java
@@ -0,0 +1,13 @@
+package com.example.kafka_test1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaTest1Application.class, args);
+	}
+
+}
---
+++ kafka-test1/kafka-test1/src/test/java/com/example/kafka_test1/KafkaTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafka_test1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
